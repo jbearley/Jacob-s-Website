2ba47de4b33a885f6c3c3b54e6edae760bdc1c48
Jacob <jacobearley01@gmail.com>
2024-02-07 21:22:28 -0600

    Update profile-dummy.js

--- profile-dummy.js
+++ profile-dummy.js
@@ -19,12 +19,14 @@ const profileData = {
     {
       school: 'Drake University',
       duration: '2020 - 2024',
+      courses: 'Computer Organization and Assembly Language, Human-Computer Interaction, Programming Languages, Software Engineering, The Theory of Computation, Vector Calculus'
+
     },
   ],
   skills: [
     {
-      category: 'Language Sklls',
-      topics: ['JavaScript', 'Python', 'Ruby', 'HTML/CSS']
+      category: 'Language/Framework Sklls',
+      topics: ['JavaScript', 'Python', 'Ruby', 'HTML/CSS', 'Node.js']
     },
     {
       category: 'Soft SKills',
@@ -34,7 +36,7 @@ const profileData = {
     },
     {
       category: 'Productivity Skills',
-      topics: ['git', 'Microsoft Applications', 'Node.js']
+      topics: ['Git', 'Microsoft Office', 'Node.js', 'Postgres']
     }
   ],
   projects: [
@@ -42,25 +44,34 @@ const profileData = {
       title: 'Smily Face Web Extension',
       duration: 'Dec - Dec 2023',
       link: 'https://github.com/jbearley/webExtension',
-      desc: `My project`
+      desc: `- Developed an in-depth understanding surrounding JSON and its use in Browser Extensions.\n
+      - Used HTML, CSS, and Javascript to create a functional multi-platform extension to create an image on any compatible webpage.\n
+      `
     },
     {
       title: 'Pokemon Team Builder',
       duration: 'Dec - Dec 2023',
       link: 'https://github.com/jbearley/pokemon',
-      desc: `My project`
+      desc: `-Inspired by the popular TV series and trading card game, this platform allows the user to select from over 500 types of pokemon to create a personalized team.\n
+      -Usage of PostgreSQL, a database management system, for RESTful backend API services to store API pulls for the pokemon images, statistics, and accessory details.\n
+      -Usage of Unit Test to optimize performance of code and prevent failure in the pipeline.\n
+      `
     },
     {
       title: '2d Engine',
       duration: 'Dec - Dec 2023',
       link: 'https://github.com/jbearley/2d_engine',
-      desc: `My project`
+      desc: `-Individually designed computer generated objects and environment using Ruby in order to simulate similar conditions of air resistance and projectiles launched from a constant angle.\n
+      -Gained a better ability to implement Newtonian Physics in a Euclidean space using variables such as Gravity, Velocity, Acceleration required to demonstrate realistic motion.\n
+      `
     },
     {
       title: 'Music Rec',
       duration: 'Dec - Dec 2023',
       link: 'https://github.com/jbearley/music_rec',
-      desc: `My project`
+      desc: `-Collaborated with teammates to develop a Ruby program that recommends 5 songs to a user based on the genre selected and the randomization of the 25 song pool selected.\n
+      -Developed an engine using a collaborative filtering algorithm.\n 
+      `
     }
   ],
   experiences: [
